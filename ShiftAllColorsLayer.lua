7ca8e95ad05572f6e5086b7588f9c1e4ace93c60
Diego Floor <diego.floor@gmail.com>
2021-07-06 16:11:33 -0300

    Add files via upload

--- ShiftAllColorsLayer.lua
+++ ShiftAllColorsLayer.lua
@@ -1,15 +1,25 @@
 
 
 
-local cel = app.activeCel
+-- -- select second layer
+-- for i,layer in ipairs(app.activeSprite.layers) do
+-- 	if(layer.stackIndex==2)then
+-- 		app.activeCel = layer:cel(1);
+-- 	end
+-- end
+
+
+
+-- local cel = app.activeCel
+local cel = app.activeLayer:cel(1)
 local cx0 = cel.position.x
 local cy0 = cel.position.y
-local cx1 = cel.bounds.width + cx0
-local cy1 = cel.bounds.height + cy0
+local cx1 = cel.bounds.width + cx0 
+local cy1 = cel.bounds.height + cy0 
 
 
 local curImg = cel.image
-
+local imgclone = curImg:clone()
 
 function PutPixel(x,y,c)
 	app.useTool{
@@ -21,42 +31,64 @@ function PutPixel(x,y,c)
 		layer=cel.layer,
 		frame=cel.frame
 	}
+	
 end
 
 
-for tx = 0, cx1, 8 do
-	for ty = 0, cy1, 8 do
-		--check if this tile has anything
-		local present = false
-		for x = tx, tx+7, 1 do
-			for y = ty, ty + 7, 1 do
-				if( x >= cx0 and y >= cy0 and x < cx1 and y < cy1) then
-					if(curImg:getPixel(x-cx0,y-cy0)>0) then
-						present = true
-						break
+function ProcessImage()
+
+	for tx = 0, cx1+cx0, 8 do
+		for ty = 0, cy1+cy0, 8 do
+			--check if this tile has anything
+			local present = false
+			
+			for x = tx, tx+7, 1 do
+				for y = ty, ty + 7, 1 do
+					if( x >= cx0 and y >= cy0 and x < cx1 and y < cy1) then
+						if(imgclone:getPixel(x-cx0,y-cy0)>0) then
+							present = true
+							-- print("tile present in "..x..", "..y..": "..imgclone:getPixel(x-cx0,y-cy0))
+							-- print("tx: "..tx..", ty: "..ty)
+							
+							break
+						end
 					end
 				end
+				if(present)then
+					break
+				end
 			end
+
+			
 			if(present)then
-				break
+				--process this tile
+				for xx = tx, tx + 7, 1 do
+					for yy = ty, ty + 7, 1 do
+						if( xx >= cx0 and yy >= cy0 and xx < cx1 and yy < cy1) then
+							
+							if(imgclone:getPixel(xx-cx0,yy-cy0)>0) then
+								PutPixel(xx,yy, imgclone:getPixel(xx-cx0,yy-cy0)+128)
+														
+							else								
+								PutPixel(xx,yy,128)
+							end
+						else							
+							PutPixel(xx,yy,128)							
+						end
+						
+					end				
+				end		
 			end
 		end
 		
-		if(present)then
-			--process this tile
-			for x = tx, tx + 7, 1 do
-				for y = ty, ty + 7, 1 do
-					if( x >= cx0 and y >= cy0 and x < cx1 and y < cy1) then
-						if(curImg:getPixel(x-cx0,y-cy0)<16)then
-							PutPixel(x,y,curImg:getPixel(x-cx0,y-cy0)+128)
-						end
-					else 
-						PutPixel(x,y,128)
-					end
-				end				
-			end		
-		end
 	end
+	
 end
---app.activeCel = cel
-app.refresh()
+
+
+app.transaction(	
+	function()
+		ProcessImage()
+	end
+)
+
